342d1c2902b3abada9aefceafc675001fde4af4c
Jo Jo Lam <jojilam@gmail.com>
2022-04-15 22:53:15 -0700

    on 14

---
+++ resources/views/posts.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+<script src="/app.js"></script>
+
+<body>
+<?php foreach ($posts as $post) : ?>
+    <article>
+        <h1><a href="/posts/<?= $post->slug; ?>"><?= $post->title; ?></a></h1>
+        <div>
+            <?= $post->excerpt; ?>
+        </div>
+    </article>
+<?php endforeach; ?>
+</body>
