3571b076b23eb54bba49ab67b275df7b699cb7f1
Jo Jo Lam <jojilam@gmail.com>
2022-04-17 23:25:20 -0700

    zzzzzz

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,18 @@
-@extends('layout')
+<x-layout>
+    @include('_posts-header')
 
-@section('banner')
-    <h1>MY BLOG?!</h1>
-@endsection
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
 
-@section('content')
-    @foreach ($posts as $post)
-        <article>
-            <h1><a href="/posts/{{$post->slug}}">
-                    {{$post->title}}</a></h1>
-            <p>
-                By <a href="/authors/{{$post->author->username}}">{{ $post->author->name }}</a> in <a
-                    href="/categories/{{$post->category->slug}}"> {{$post->category->name}}</a>
-            </p>
-            <div>
-                {{$post->excerpt}}
-            </div>
-        </article>
-        @endforeach
-        </body>
-@endsection
+
+            <x-post-grid :posts="$posts"/>
+        @else
+            <p class="text-center">No posts yet!!!!!!</p>
+        @endif
+        <div class="lg:grid lg:grid-cols-3">
+
+
+        </div>
+    </main>
+
+</x-layout>
